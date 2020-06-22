2e09bc71ce707d49103615a8644568d52e318b70
nangongyibin <nangongyibin@gmail.com>
2020-06-22 21:28:20 +0800

    静态资源包访问修改

---
+++ src/main/java/com/ngyb/takeout/WebConfig.java
@@ -0,0 +1,21 @@
+package com.ngyb.takeout;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 作者：南宫燚滨
+ * 描述：
+ * 邮箱：nangongyibin@gmail.com
+ * 日期：2020/6/22 20:23
+ */
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //将所有/static/** 访问都映射到classpath:/static/ 目录下
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+}
